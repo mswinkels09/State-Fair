db8f67c9d7c4b231fda8779b89a1e1473cd444a0
mswinkels09 <mswinkels09@gmail.com>
2020-07-27 16:10:11 -0500

    added buttons to food, game, ride, and sideshow

--- scripts/TicketBooth.js
+++ scripts/TicketBooth.js
@@ -1,13 +1,57 @@
+import { RideTicketHolders } from "./rides/RideTicketHolders.js";
+
 const contentTarget = document.querySelector(".entry")
+
 const eventHub = document.querySelector("#state-fair")
 
-eventHub.addEventListener()
+
+eventHub.addEventListener("click", clickEvent => {
+    if (clickEvent.target.id === "rideTicket") {       
+        const ticketEvent = new CustomEvent("rideTicketPurchased")
+        eventHub.dispatchEvent(ticketEvent)}
+        })
+
+eventHub.addEventListener("click", clickEvent => {
+    if (clickEvent.target.id === "foodTicket") {       
+        const ticketEvent = new CustomEvent("foodTicketPurchased")
+        eventHub.dispatchEvent(ticketEvent)}
+                })
+
+eventHub.addEventListener("click", clickEvent => {
+    if (clickEvent.target.id === "gameTicket") {       
+        const ticketEvent = new CustomEvent("gameTicketPurchased")
+        eventHub.dispatchEvent(ticketEvent)}
+                })
+
+eventHub.addEventListener("click", clickEvent => {
+    if (clickEvent.target.id === "sideshowTicket") {       
+        const ticketEvent = new CustomEvent("sideshowTicketPurchased")
+        eventHub.dispatchEvent(ticketEvent)}
+                })
+
+eventHub.addEventListener("click", clickEvent => {
+    if (clickEvent.target.id === "fullPackageTicket") {
+               
+        const contentTarget = document.querySelectorAll(".people");
+        
+        const ticketEvent = new CustomEvent("fullPackageTicketPurchased")
+        
+        contentTarget.innerHTML += `<div class="person bigSpender"></div>`
+        
+        eventHub.dispatchEvent(ticketEvent)
+    }
+                })
+        
+
 
 export const TicketBooth = () => {
     contentTarget.innerHTML = `
         <div class="ticketBooth">
-            Add button here
+        <button id="rideTicket">Ride Ticket</button>
+        <button id="foodTicket">Food Ticket</button>
+        <button id="gameTicket">Game Ticket</button>
+        <button id="sideshowTicket">Sideshow Ticket</button>
+        <button id="fullPackageTicket">Full Package Ticket</button>
         </div>
     `
 }
-
---
+++ scripts/games/GameTicketHolders.js
@@ -0,0 +1,8 @@
+const contentTarget = document.querySelector(".games")
+const eventHub = document.querySelector("#state-fair")
+
+export const GameTicketHolders = () => {
+    eventHub.addEventListener("gameTicketPurchased", customEvent => {
+            contentTarget.innerHTML += `<div class="person player"></div>`
+    })
+}
